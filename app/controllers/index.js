56bbcdf3fb6489c3a7e050b78c0fc089045f3b10
Himalay <contact@himalay.com.np>
2015-09-07 13:53:25 +0545

    add tasks in update() to clear textfield after value set to label

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,23 +1,27 @@
-    function doClick(e) {
+function doClick(e) {
     if ($.title.value && $.deadline.value){ // checks if both TextFields has values
-        $.projectTitle.text = $.title.value;
-        $.projectDeadline.text = $.deadline.value;
-    }
-    }
-    
-     function update() {
-    if ($.title2.value && $.deadline2.value && $.tasks2.value){ // checks if both TextFields has values
-        $.projectTitle2.text = $.title2.value;
-        $.projectDeadline2.text = $.deadline2.value;
-        $.projectTasks2.text = $.tasks2.value;
-    }
-    }
-    
-    function edit(){
-    	$.title2.value = $.projectTitle2.text;
-        $.deadline2.value = $.projectDeadline2.text;
-        $.tasks2.value = $.projectTasks2.text;
-    }
-    
-    $.index.open();
-    
+	    $.projectTitle.text = $.title.value;
+	    $.title.value = ''; // empty textfield when the value is set to label
+	    $.projectDeadline.text = $.deadline.value;
+	    $.deadline.value = '';
+	}
+}
+
+function update() {
+	if ($.title2.value && $.deadline2.value && $.tasks2.value){ // checks if both TextFields has values
+    	$.projectTitle2.text = $.title2.value;
+    	$.title2.value = '';
+    	$.projectDeadline2.text = $.deadline2.value;
+    	$.deadline2.value = '';
+    	$.projectTasks2.text = $.tasks2.value;
+    	$.tasks2.value = '';
+	}
+}
+
+function edit(){
+	$.title2.value = $.projectTitle2.text;
+    $.deadline2.value = $.projectDeadline2.text;
+    $.tasks2.value = $.projectTasks2.text;
+}
+
+$.index.open();
