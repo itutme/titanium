7343592f990acc48029fd90316fbe61700626afd
Himalay <contact@himalay.com.np>
2015-09-07 13:58:01 +0545

    add task to doClick() to clear textfield after label text is set

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,9 @@
     function doClick(e) {
     if ($.title.value && $.deadline.value){ // checks if both TextFields has values
         $.projectTitle.text = $.title.value;
+        $.title.value = ''; // empty textfield when the value is set to label
         $.projectDeadline.text = $.deadline.value;
+        $.deadline.value = '';
     }
     }
     
