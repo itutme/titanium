f2458e4ba688eb795e962ce9f610f48a637e964f
Himalay <contact@himalay.com.np>
2015-09-07 13:36:39 +0545

    add Day one challenge solution

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,7 +5,19 @@
     }
     }
     
-// alert(JSON.stringify($.projectTitle))
-
+     function update() {
+    if ($.title2.value && $.deadline2.value && $.tasks2.value){ // checks if both TextFields has values
+        $.projectTitle2.text = $.title2.value;
+        $.projectDeadline2.text = $.deadline2.value;
+        $.projectTasks2.text = $.tasks2.value;
+    }
+    }
+    
+    function edit(){
+    	$.title2.value = $.projectTitle2.text;
+        $.deadline2.value = $.projectDeadline2.text;
+        $.tasks2.value = $.projectTasks2.text;
+    }
+    
     $.index.open();
     
