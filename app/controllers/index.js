25d8d5b91a0d9cd87817321cb560b9b51b6b8a71
Himalay <contact@himalay.com.np>
2015-09-08 15:41:46 +0545

    impliment tss and update element ids

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,27 +1,18 @@
-function doClick(e) {
-    if ($.title.value && $.deadline.value){ // checks if both TextFields has values
-	    $.projectTitle.text = $.title.value;
-	    $.title.value = ''; // empty textfield when the value is set to label
-	    $.projectDeadline.text = $.deadline.value;
-	    $.deadline.value = '';
-	}
-}
-
 function update() {
-	if ($.title2.value && $.deadline2.value && $.tasks2.value){ // checks if both TextFields has values
-    	$.projectTitle2.text = $.title2.value;
-    	$.title2.value = '';
-    	$.projectDeadline2.text = $.deadline2.value;
-    	$.deadline2.value = '';
-    	$.projectTasks2.text = $.tasks2.value;
-    	$.tasks2.value = '';
+	if ($.projectTitleInput.value && $.projectDeadlineInput.value && $.projectTasksInput.value){ // checks if both TextFields has values
+    	$.projectTitle.text = $.projectTitleInput.value;
+    	$.projectTitleInput.value = '';
+    	$.projectDeadline.text = $.projectDeadlineInput.value;
+    	$.projectDeadlineInput.value = '';
+    	$.projectTasks.text = $.projectTasksInput.value;
+    	$.projectTasksInput.value = '';
 	}
 }
 
 function edit(){
-	$.title2.value = $.projectTitle2.text;
-    $.deadline2.value = $.projectDeadline2.text;
-    $.tasks2.value = $.projectTasks2.text;
+	$.projectTitleInput.value = $.projectTitle.text;
+    $.projectDeadlineInput.value = $.projectDeadline.text;
+    $.projectTasksInput.value = $.projectTasks.text;
 }
 
 $.index.open();
