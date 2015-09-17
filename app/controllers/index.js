8ca159db34a39f0983145d53b2bb63a67c1ed644
Himalay <contact@himalay.com.np>
2015-09-17 19:04:03 +0545

    Display start date and softred color for deadline date

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,22 +1,15 @@
-var moment = require('alloy/moment');
-var Projects = Alloy.Collections.projects;
+var moment = require('alloy/moment'); //The this returns an object, with public properties and functions of momentjs
+var projects = Alloy.Collections.projects;
 
-Projects.fetch();
+projects.fetch();
 
 $.index.open();
 
-function filterFunction(collection) {
-   	return collection.where({stage: 'open'});
-   }
-
 function transformFunction(model) {
-    var transform = model.toJSON();
+    var transform = model.toJSON(); // converts into JSON format so that we can work with it
     var people = transform.assigned_to;
-	transform.end_dt = moment(transform.end_dt).format('ll');
-    transform.assigned_to = people && people.length;
-    return transform;
-}
-
-function refreshTable(e) {
-	Projects.fetch();
+    transform.start_dt = moment(transform.start_dt).format('ll'); // return date in format of "Sep 17 2015"
+	transform.end_dt = moment(transform.end_dt).format('ll'); // return date in format of "Sep 17 2015"
+    transform.assigned_to = people && people.length || 0; // checks if project is assigned to any one and retuns the count
+    return transform; // returns the updated collection
 }
