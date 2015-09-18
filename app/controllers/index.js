758582195a58782062992cf0e319d1adcf2a7af4
Himalay <contact@himalay.com.np>
2015-09-18 18:38:36 +0545

    Add tabs for open projects and done projects

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,22 +1,23 @@
-var moment = require('alloy/moment');
-var Projects = Alloy.Collections.projects;
+var moment = require('alloy/moment'); //The this returns an object, with public properties and functions of momentjs
+var projects = Alloy.Collections.projects;
 
-Projects.fetch();
+projects.fetch();
 
 $.index.open();
 
-function filterFunction(collection) {
+function transformFunction(model) {
+    var transform = model.toJSON(); // converts into JSON format so that we can work with it
+    var people = transform.assigned_to;
+    transform.start_dt = moment(transform.start_dt).format('ll'); // return date in format of "Sep 17 2015"
+	transform.end_dt = moment(transform.end_dt).format('ll'); // return date in format of "Sep 17 2015"
+    transform.assigned_to = people && people.length || 0; // checks if project is assigned to any one and retuns the count
+    return transform; // returns the updated collection
+}
+
+function filterOpen(collection) {
    	return collection.where({stage: 'open'});
    }
-
-function transformFunction(model) {
-    var transform = model.toJSON();
-    var people = transform.assigned_to;
-	transform.end_dt = moment(transform.end_dt).format('ll');
-    transform.assigned_to = people && people.length;
-    return transform;
-}
-
-function refreshTable(e) {
-	Projects.fetch();
-}
+   
+function filterDone(collection) {
+   	return collection.where({stage: 'done'});
+   }
