49191a1c6f4eef5303353382ee91d1bddb2fb10b
Himalay <contact@himalay.com.np>
2015-09-27 15:36:57 +0545

    Nicely format code

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,5 @@
-var moment = require('alloy/moment'); //The this returns an object, with public properties and functions of momentjs
+var moment = require('alloy/moment');
+//The this returns an object, with public properties and functions of momentjs
 var projects = Alloy.Collections.projects;
 
 projects.fetch();
@@ -6,18 +7,27 @@ projects.fetch();
 $.index.open();
 
 function transformFunction(model) {
-    var transform = model.toJSON(); // converts into JSON format so that we can work with it
+    var transform = model.toJSON();
+    // converts into JSON format so that we can work with it
     var people = transform.assigned_to;
-    transform.start_dt = moment(transform.start_dt).format('ll'); // return date in format of "Sep 17 2015"
-	transform.end_dt = moment(transform.end_dt).format('ll'); // return date in format of "Sep 17 2015"
-    transform.assigned_to = people && people.length || 0; // checks if project is assigned to any one and retuns the count
-    return transform; // returns the updated collection
+    transform.start_dt = moment(transform.start_dt).format('ll');
+    // return date in format of "Sep 17 2015"
+    transform.end_dt = moment(transform.end_dt).format('ll');
+    // return date in format of "Sep 17 2015"
+    transform.assigned_to = people && people.length || 0;
+    // checks if project is assigned to any one and retuns the count
+    return transform;
+    // returns the updated collection
 }
 
 function filterOpen(collection) {
-   	return collection.where({stage: 'open'});
-   }
-   
+    return collection.where({
+        stage : 'open'
+    });
+}
+
 function filterDone(collection) {
-   	return collection.where({stage: 'done'});
-   }
+    return collection.where({
+        stage : 'done'
+    });
+}
